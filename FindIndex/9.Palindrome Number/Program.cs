4d3a820b569086032a7118da8f31051505d57670
Andrey <karl911291@mail.ru>
2022-11-24 23:10:32 +0300

    Complete task №9
    Palindrome Number

---
+++ FindIndex/9.Palindrome Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(IsPolyndrom(1211));
+        }
+        static bool IsPolyndrom(int value)
+        {
+            string str = value.ToString();
+            int j = str.Length - 1;
+
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                if (str[i] == str[j])
+                {
+                    j--;
+                    continue;
+                }
+                else
+                    return false;
+                j--;
+            }
+            return true;
+        }
+    }
+}
